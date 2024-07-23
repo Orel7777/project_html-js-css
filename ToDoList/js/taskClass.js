ebb87c950e698d66a9dcff3a6ab364216c356262
Orel bukris <orelbukris7777@gmail.com>
2024-07-23 13:55:19 +0300

    project

--- ToDoList/js/taskClass.js
+++ ToDoList/js/taskClass.js
@@ -1,25 +1,25 @@
 export default class Task {
-    constructor(_parent, _item, _delSingleTask) {
+  constructor(_parent, _item, _delSingleTask) {
       this.parent = _parent;
       this.name = _item.name;
       this.time = _item.time;
       this.id = _item.id;
       this.delSingleTask = _delSingleTask; 
-    }
-  
-    render() {
+  }
+
+  render() {
       let div = document.createElement("div");
-      div.className = "shadow-sm my-2 p-2";
+      div.className = "shadow-sm my-2 p-2 d-flex justify-content-between align-items-center";
       document.querySelector(this.parent).append(div);
       div.innerHTML += `
-        <button class="btn btn bg-danger float-end">X</button>
-        <h4>${this.name} - ${this.time}</h4>
+          <h4 class="mb-0">${this.name} - ${this.time}</h4>
+          <button class="btn btn-danger btn-sm" style="width: 50px;">X</button>
       `;
       let btnX = div.querySelector("button");
       btnX.addEventListener("click", () => {
-        window.confirm("Are you sure you want to delete this task");
-        this.delSingleTask(this.id); 
+          if (window.confirm("Are you sure you want to delete this task")) {
+              this.delSingleTask(this.id); 
+          }
       });
-    }
   }
-  
+}
